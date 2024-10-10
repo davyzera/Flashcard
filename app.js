a66c00fe5d0213d92b35034a98c4af8469c51d82
davyzera <164011746+davyzera@users.noreply.github.com>
2024-10-10 11:58:05 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-
     cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
@@ -15,14 +14,12 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     `
     let respostaEstaVisivel = false
-
+    
     function viraCartao(){
         respostaEstaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
     }
     
     cartao.addEventListener('click', viraCartao)
-
     container.appendChild(cartao)
-
 }
